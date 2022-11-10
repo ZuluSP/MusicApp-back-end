584ab41540c4f29dbbcf0e00b579b2e045c21a0e
Carlos Zulueta <zulueta.demadariaga.carlos@gmail.com>
2022-11-10 21:15:14 +0100

    reformat: deleted unnecessary entity

--- src/spotlist/spotlist.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, Post, Put, Delete, Res } from '@nestjs/common';
-
-@Controller('spotlist')
-export class SpotlistController {
-  @Post('/create')
-  createSong(@Res() res) {
-    res.status();
-  }
-}
--- src/spotlist/spotlist.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpotlistController } from './spotlist.controller';
-import { SpotlistService } from './spotlist.service';
-
-@Module({
-  controllers: [SpotlistController],
-  providers: [SpotlistService]
-})
-export class SpotlistModule {}
